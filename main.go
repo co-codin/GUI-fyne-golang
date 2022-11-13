3a0ae57b792f2a41cb7dc4248b34d2cd319e0722
Yeqing <cuiyeqing960904@gmail.com>
2022-11-13 15:49:59 -0500

    client get req

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+	"time"
 
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
@@ -9,7 +11,28 @@ import (
 
 var client *http.Client
 
+type randomFact struct {
+	Text string `json:"text"`
+}
+
+func getRandomFact() (randomFact, error) {
+	var fact randomFact
+	resp, err : = client.Get("https://uselessfacts.jsph.pl/random.json?language=en")
+	if err != nil {
+		return randomFact{}, err
+	}
+	defer resp.Body.close()
+
+	err = json.NewDecoder(resp.Body).Decode(&fact)
+
+	if err != nil {
+		return randomFact{}, err
+	}
+	return fact, nil
+}
+
 func main() {
+	client = &http.Client{Timeout: 10 * time.Second}
 	a := app.New()
 	w := a.NewWindow("Hello World")
 
